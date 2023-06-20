eb695e5507b86fe8eb4a2a79204e238765e3e43e
aryabimamp <aryabimamanda@gmail.com>
2023-06-20 13:37:01 +0800

    alpha beta fix

--- src/policy/alphabeta.cpp
+++ src/policy/alphabeta.cpp
@@ -13,42 +13,34 @@
  * @return Move 
  */
 
-int value;
-
 int abpruning(State* node, int depth, int max, int alpha, int beta){
-  if(depth==0){
+  if(!node->legal_actions.size()){
+      node->get_legal_actions();
+  }
+
+  if(depth==0||node->legal_actions.size()==0){
     return node->evaluate();
   }
 
-  if(max==1){
-    value = -1000;
-    if(!node->legal_actions.size()){
-      node->get_legal_actions();
-    }
-    auto legal = node->legal_actions;
-    
-    for(auto i : legal){
-      State* changestate = node->next_state(i);
+  auto legal = node->legal_actions;
+  int value = (max) ? -1000 : 1000;
+
+  if(max){
+    for(auto moves : legal){
+      State* changestate = node->next_state(moves);
       value = std::max(value, abpruning(changestate,depth-1,0,alpha,beta));
       alpha = std::max(alpha, value);
-      if(alpha>=beta){
+      if(alpha>beta){
         break;
       }
     }
     return value;
   }
-
-  else if(max==0){
-    value = 1000;
-    if(!node->legal_actions.size()){
-      node->get_legal_actions();
-    }
-    auto legal = node->legal_actions;
-    for(auto i : legal){
-      State* changestate = node->next_state(i);
+  else{
+    for(auto moves : legal){
+      State* changestate = node->next_state(moves);
       value = std::min(value, abpruning(changestate,depth-1,1,alpha,beta));
-      beta = std::min(beta, value);
-      if(beta<=alpha){
+      if(alpha<beta){
         break;
       }
     }
@@ -61,35 +53,35 @@ Move Alphabeta::get_move(State *state, int depth){
   if(!state->legal_actions.size())
     state->get_legal_actions();
     
-  auto actions = state->legal_actions;
+auto actions = state->legal_actions;
 Move k;
-int player = state->player;
-int w = (player==1)? 1000 : -1000;
-int ev;
-if(player==1){
-  for(auto i : actions){
+int whiteorblack = (!state->player) ? -1000 : 1000;
 
-  State* changestate = state->next_state(i);
+if(!state->player){
+  for(auto moves : actions){
 
-  ev = abpruning(changestate,depth-1,0,-1000,1000);
-    if(ev < w){
-    w = ev;
-    k = i;
+  State* changestate = state->next_state(moves);
+
+  int eval = abpruning(changestate,depth-1,0,-1000,1000);
+    if(eval > whiteorblack){
+    whiteorblack = eval;
+    k = moves;
     }
   }
 return k;
 }
 else{
-  for(auto i : actions){
+  for(auto moves : actions){
+  
+  State* changestate = state->next_state(moves);
 
-  State* changestate = state->next_state(i);
-
-  ev = abpruning(changestate,depth-1,1,-1000,1000);
-   if(ev > w){
-   w = ev;
-   k = i;
+  int eval = abpruning(changestate,depth-1,1,-1000,1000);
+   if(eval < whiteorblack){
+   whiteorblack = eval;
+   k = moves;
    }
   }
 return k;
 }
+
 }
